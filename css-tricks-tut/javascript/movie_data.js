81cc4a01ee70852ce98089fd58633241204ca363
Randy Davis <randy_davis@outlook.com>
2015-01-07 20:31:08 -0600

    css-tricks-tut folder and finishes js template example using jquery and underscore after an error in cli

---
+++ css-tricks-tut/javascript/movie_data.js
@@ -0,0 +1,12 @@
+var data = {
+  "movies": [
+    {
+      "movie_title": "Commando",
+      "movie_desc": "Arnold kicks butt."
+    },
+    {
+      "movie_title": "Jaws",
+      "movie_desc": "A big shark kicks butt."
+    }
+  ]
+};
